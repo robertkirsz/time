e369df205342dda6da76aad1581f7f4c791bf2bb
Robert Kirsz <robert.kirsz@gmail.com>
2017-02-18 14:50:25 +0100

    Added locale detection

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import moment from 'moment'
 import App from './App'
+
+const getLang = () => (
+  navigator.languages !== undefined
+    ? navigator.languages[0]
+    : navigator.language
+)
+
+moment.locale(getLang())
 
 /* eslint-disable no-new */
 new Vue({
